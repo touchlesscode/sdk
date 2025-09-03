65ccd5d6412edf164cc05108085156ebd7771e52
Kevin Gervais <kevin@gervais.is>
2025-09-02 23:59:16 -0400

    Add sample GTM

--- starters/next-app-router/src/app/layout.tsx
+++ starters/next-app-router/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -27,10 +28,23 @@ export default function RootLayout({
       <link rel="preconnect" href="https://exo.touchlessapis.com" />
       <link rel="dns-prefetch" href="https://exo.touchlessapis.com" />
       <head>
-        <script 
-          src="https://exo.touchlessapis.com" 
-          defer={false}
-        ></script>
+         {/* EXO script as high as possible */}
+        <script src="https://exo.touchlessapis.com" defer={false}></script>
+
+        {/* GTM bootstrap snippet */}
+        <Script id="gtm-script" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TXC85LT');
+          `}
+        </Script>
+
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
